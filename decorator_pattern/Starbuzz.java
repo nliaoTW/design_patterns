12fbe62f204c5db8e39e9ccd09dae8fb5d810d94
Nelson Liao <nliao@thoughtworks.com>
2021-08-09 20:09:45 -0700

    implement decorator pattern

---
+++ decorator_pattern/Starbuzz.java
@@ -0,0 +1,13 @@
+public class Starbuzz {
+    public static void main(String args[]) {
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+
+        Beverage beverage1 = new HouseBlend();
+        beverage1 = new Mocha(beverage1);
+        beverage1 = new Mocha(beverage1);
+        beverage1 = new Soy(beverage1);
+        beverage1 = new Whip(beverage1);
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+    }
+}
